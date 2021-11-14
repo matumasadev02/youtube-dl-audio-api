c6e6af2e7eb0d359e2274b68e49b6efd091cbe0b
matumasadev02 <77044083+matumasadev02@users.noreply.github.com>
2021-11-14 18:02:58 +0900

    [update] support ytsearch

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from flask import Flask, request, jsonify, redirect
 
 app = Flask(__name__)
 
+# return the audio url of the video
 def returnAudioUrl(url):
     with youtube_dl.YoutubeDL({'format': 'bestaudio/best'}) as ydl:
         result = ydl.extract_info(url,download=False)
         return (result.get('url'))
 
+# search youtube using youtube-dl and return the audio url of first result
+def search(query):
+    with youtube_dl.YoutubeDL({'format': 'bestaudio/best'}) as ydl:
+        playlist = ydl.extract_info(f'ytsearch:{query}',download=False)
+        return (playlist.get('entries')[0].get('url'))
+
+# redirect to the audio url of the video
 @app.route('/')
 def redirect_url():
     if request.args.get('url'):
@@ -16,9 +24,17 @@ def redirect_url():
             return redirect(returnAudioUrl(url))
         except Exception as e:
             return jsonify({"error": str(e)})
+    elif request.args.get('query'):
+        try:
+            query = request.args.get('query')
+            return redirect(search(query))
+        except Exception as e:
+            return jsonify({"error": str(e)})
     else:
         return jsonify({"error": "Please enter the video url. Doc: https://github.com/matumasadev02/youtube-dl-audio-api/blob/master/README.md"})
-@app.route('/get')
+
+# return the audio url of the video
+@app.route('/info')
 def return_url():
     if request.args.get('url'):
         try:
@@ -26,7 +42,14 @@ def return_url():
             return jsonify({"url": returnAudioUrl(url)})
         except Exception as e:
             return jsonify({"error": str(e)})
+    elif request.args.get('query'):
+        try:
+            query = request.args.get('query')
+            return jsonify({"url": search(query)})
+        except Exception as e:
+            return jsonify({"error": str(e)})
     else:
         return jsonify({"error": "Please enter the video url. Doc: https://github.com/matumasadev02/youtube-dl-audio-api/blob/master/README.md"})
+
 if __name__ == '__main__':
     app.run()
